ecc735f02bd5633f39bf26a70422ac6a5cd6200d
Amaia Berrocal <amaia.berrocal@gmail.com>
2022-10-26 13:37:03 +0200

    #E 7_8_9 Added test

--- exercises_4_5_6/src/test/java/com/exercises_4_5_6/controller/LaptopControllerTest.java
+++ exercises_4_5_6/src/test/java/com/exercises_4_5_6/controller/LaptopControllerTest.java
@@ -1,51 +1,83 @@
 package com.exercises_4_5_6.controller;
 
+import com.exercises_4_5_6.entities.Laptop;
 import com.exercises_4_5_6.repository.LaptopRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class LaptopControllerTest {
-    LaptopController sut;
-    LaptopRepository lr;
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
+
+    @LocalServerPort
+    private int port;
+
 
     @BeforeEach
-    void setUp() {
-        lr = mock(LaptopRepository.class);
-        sut = new LaptopController(lr);
+    void serUp() {
+        restTemplateBuilder = restTemplateBuilder.rootUri("http://localhost:" + port);
+        testRestTemplate = new TestRestTemplate(restTemplateBuilder);
     }
 
     @Test
-    //void findAll() {}
-    void should_return_a_list_of_laptops() {
-        //given
-        //when
-        sut.findAll();
-        //then
-        verify(lr).findAll();
+    void findAll() {
+        ResponseEntity<Laptop[]> response =
+            testRestTemplate.getForEntity("/laptops", Laptop[].class);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(200, response.getStatusCodeValue());
+
+        List<Laptop> laptops = Arrays.asList(response.getBody());
+        System.out.println(laptops.size());
     }
+
 
     @Test
     void findOneById() {
+        ResponseEntity<Laptop[]> response =
+                testRestTemplate.getForEntity("/laptops/1", Laptop[].class);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(200, response.getStatusCodeValue());
 
     }
 
     @Test
     void create() {
-    }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
-    @Test
-    void update() {
-    }
+        String json = """
+                {
+                "fabricante": "aaaaaaa",
+                "modelo": "eeeeee",
+                "tamaño": 45,
+                "año": 2156
+                }
+                """;
 
-    @Test
-    void delete() {
-    }
+        HttpEntity<String> request = new HttpEntity<>(json, headers);
+        ResponseEntity<Laptop> response = testRestTemplate.exchange("/laptops", HttpMethod.POST, request, Laptop.class);
 
-    @Test
-    void deleteAll() {
+        Laptop result = response.getBody();
+
+        assertEquals(1L, result.getId());
+        assertEquals("aaaaaaa", result.getFabricante());
     }
 }
